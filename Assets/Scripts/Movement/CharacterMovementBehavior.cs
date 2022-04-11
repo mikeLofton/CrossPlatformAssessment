3140c96882f10bd13ab0861614b87a0360298186
mikeLofton <michaellofton32@gmail.com>
2022-04-11 13:00:26 -0500

    Added object movement

---
+++ Assets/Scripts/Movement/CharacterMovementBehavior.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementBehavior : MovementBehavior
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private bool _movesLeft;
+    [SerializeField]
+    private bool _movesRight;
+
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+
+    public bool MovesLeft
+    {
+        get { return _movesLeft; }
+        set { _movesLeft = value; }
+    }
+
+    public bool MovesRight
+    {
+        get { return _movesRight; }
+        set { _movesRight = value; }
+    }
+
+    public override void Update()
+    {
+        if (MovesLeft)
+        {
+            Vector3 direction = Vector3.left;
+            Velocity = direction.normalized * Speed;
+        }
+
+        if (MovesRight)
+        {
+            Vector3 direction = Vector3.right;
+            Velocity = direction.normalized * Speed;
+        }
+        
+
+        base.Update();
+    }
+
+}
---
+++ Assets/Scripts/Movement/MovementBehavior.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehavior : MonoBehaviour
+{
+    private Vector3 _velocity;
+
+    public Vector3 Velocity
+    {
+        get { return _velocity; }
+        set { _velocity = value; }
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        transform.position += Velocity * Time.deltaTime;
+    }
+}
